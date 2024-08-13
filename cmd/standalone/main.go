250ed1e483bccc68ae24525b76559a2701b6d119
Matthias Bruns <git@matthiasbruns.com>
2024-08-13 19:50:22 +0200

    fix: build

---
+++ cmd/standalone/main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	// This is a placeholder for the main package
+	// This package is not intended to be used as a standalone binary
+	// It is only used for testing and debugging purposes
+}
